ff10d37584da92ecb82897ca914763921182f3de
Mac Kerb <jeangillesemmanuel96@gmail.com>
2022-12-24 15:03:43 -0500

    About me class

---
+++ src/day02_escapeSequences/About_me.java
@@ -0,0 +1,25 @@
+package day02_escapeSequences;
+/*
+ Write a code to print the following info about you:
+   1. Your favorite book
+   2. Your favorite quote
+   3. Your favorite TV show
+   4. Your favorite music
+ */
+
+public class About_me {
+    public static void main(String[] args) {
+        System.out.println("1.Your favorite book: \"The millionaire " +
+                "next door\".\n2. Your favorite quote:\"Audentes fortuna iuvat\"" +
+                "\n3. Your favorite TV show: \"Lucifer\" \n4. Your favorite music:" +
+                "\"This is how you'll be remenber\"");
+
+
+        System.out.println(1+"aws"+(2+4));
+        System.out.println('a'+'w'+'s');
+
+
+    }
+
+
+}
